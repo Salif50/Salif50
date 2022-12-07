614ef28aa25778071f21f9c41eceba4f3e3d0936
Salif50 <94130934+Salif50@users.noreply.github.com>
2022-12-07 14:14:26 -0800

    Create terminal.sh

---
+++ Ressources/terminal/terminal.sh
@@ -0,0 +1,6 @@
+ls #lister le contenu d'un repertoire
+mkdir #creer un repertoire
+cd # se deplacer dans un repertoire*
+mv #renommer ou deplacer un fichier ou repertoire
+rm #suprimer un fichier (option -R pour suprimer un repertoire)
+chmod #changer le droit d'un fichier ou repertoire
