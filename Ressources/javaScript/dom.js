8f8a0d569d486989851e3c456abfd3a72b64f67b
Salif50 <94130934+Salif50@users.noreply.github.com>
2022-12-08 17:24:00 +0100

    Create dom.js

---
+++ Ressources/javaScript/dom.js
@@ -0,0 +1,19 @@
+getElementById('yourId') /* retourne l'element qui a identifiant yourId    */;
+getElementsByClassName('className') /* retourne une collection d'element ayant la classe className */
+getElementByTagName('tagHTML') /* retourne la balise HTML */
+querySelector('selector css') /* retoure le premier element correspondant au selecteur specifiés */
+querySelectorAll('selector css') /* retoure tous les elements correspondant au(x) selecteur specifiés */
+lastElementChild() /* recupère le dernier enfant de l'element */
+lastChild() /* recupère le dernier noeud enfant de l'element */
+nextSibling() /* obtient le noeud suivant situé au même niveau */
+nextElementSibling() /* obtient l'element suivant situé au même niveau de noeud */
+nodeName() /* recupère le nom du noeud courant */
+parentNode() /* recupère le noeud parent de l'element */
+parentElement() /* recupère l'élément parent de l'élément */
+previousSibling() /* obtient le noeud précédent situé au même niveau */
+previousElementSibling() /* obtient l'élément précédent situé au même niveau de noeud */
+childElementCount() /* récupère le nombre d'élément enfants dans le noeud spécifié */
+childNodes() /* récupère une liste des noeuds enfants de l'élément */
+classList() /* récupère une liste ou les noms de classe de l'élément */
+className() /* récupère ou definit la valeur de l'attribut de classe de l'élément */
+
